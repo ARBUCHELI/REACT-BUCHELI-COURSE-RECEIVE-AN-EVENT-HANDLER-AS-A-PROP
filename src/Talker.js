73fadf7a382e0cca2b874f050c20a1faaf5359a4
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-11-09 09:25:29 +0300

    Add files via upload

---
+++ src/Talker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from './Button';
+
+export class Talker extends React.Component {
+  handleClick() {
+    let speech = '';
+    for (let i = 0; i < 10000; i++) {
+      speech += 'blah ';
+    }
+    alert(speech);
+  }
+  
+  render() {
+    return <Button talk={this.handleClick}/>;
+  }
+}
